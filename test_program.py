a6a1a88d7604f8788137dc007a07e2d0e54ba89f
Dmitry Nikiforov <dima.nicki2013@yandex.ru>
2025-06-23 11:32:37 +0300

    d

--- test_program.py
+++ test_program.py
@@ -15,6 +15,6 @@ def test_program():
     except Exception as e:
         assert False, (
             'Не удалось запустить `program.py`. '
-            'Исправьте в нем ошибки:\n'
+            'Исправьте в нем ошибк и:\n'
             f'{e}'
         )
